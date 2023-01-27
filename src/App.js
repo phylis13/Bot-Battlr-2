2f893ec68f450b9d87ddb927ce02d670dcfed28d
phylis13 <phylisnjeri54@gmail.com>
2023-01-27 13:22:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import BotsDisplay from './Components/BotDisplay';
+
+function App() {
+  return (
+    <div className="App">
+      <BotsDisplay/>
+    </div>
+  );
+}
+
+export default App;
